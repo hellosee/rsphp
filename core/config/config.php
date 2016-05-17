0c940a3e70181a7d686863c003ece09a9d70d5ad
hellosee <1335244575@qq.com>
2016-05-17 20:20:19 +0800

    提交版本
    提交新版本

--- core/config/config.php
+++ core/config/config.php
@@ -1,9 +1,7 @@
 <?php
-$GLOBALS['config']['default_controller'] = 'index';
-$GLOBALS['config']['default_action'] = 'index';
+$GLOBALS['config']['default_controller'] = 'index';//默认控制器
+$GLOBALS['config']['default_action'] = 'index';//默认操作方法名
 $GLOBALS['config']['default_language'] = 'zh_cn';
 $GLOBALS['config']['url_model'] = '0';//url路由，0=参数模式，1=pathinfo rewirte模式
-//模板文件后缀名
-$GLOBALS['config']['tpl_ext'] = '.php';
-//日志目录
-$GLOBALS['config']['log_path'] = APP_PATH.'runtime'.DS.'logs'.DS;
+$GLOBALS['config']['tpl_ext'] = '.php';//模板文件后缀名
+$GLOBALS['config']['log_path'] = APP_PATH.'runtime'.DS.'logs'.DS;//日志目录
